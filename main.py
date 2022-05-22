b6084ea08186ff5a641f6c1df906cfdf1469dcc1
Timur Belkin <timurbelkin@gmail.com>
2022-05-22 07:44:23 +0300

    Added ascending and descending

--- main.py
+++ main.py
@@ -15,24 +15,34 @@ ser2 = pandas.Series(sampl, copy=False)
 ser2.drop_duplicates()
 countOfDublicates = ser.count() - ser2.count()
 
-print(sampl)
+print(f'Sample: {sampl}')
 print(f'Value min: {minValue}, value max {maxValue}, value sum {sumValue}, count of dublicates {countOfDublicates}')
-
-print (f'Test {ser.count()}')
 
 plot1 = plt.figure(1)
 arrayX = list(range(1, numberOfElement + 1))
 plt.plot(arrayX, ser)
-plt.title('title name')
-plt.xlabel('xAxis name')
-plt.ylabel('yAxis name')
+plt.title('Result')
+plt.xlabel('Numbers')
+plt.ylabel('Values')
 
 plot2 = plt.figure(2)
 serFormatted = pandas.Series([float(round(elem, 2)) for elem in sampl], copy=False)
 serFormatted.plot.hist(grid=True, bins=20, rwidth=0.9,
                    color='#607c8e')
-plt.title('Histograma for ')
+plt.title('Histogram for values')
 plt.xlabel('Counts')
 plt.ylabel('Values')
 plt.grid(axis='y', alpha=0.75)
+
+
+serDesc = ser.sort_values(ascending=False)
+serAsc = ser.sort_values()
+
+plot3 = plt.figure(3)
+plt.plot(arrayX, serAsc)
+plt.plot(arrayX, serDesc)
+plt.title('Ascending and descending')
+plt.xlabel('Number')
+plt.ylabel('Value')
+
 plt.show()
